945454434f88e0567199930cd5d343c7684c9c2c
ronrix <medyochain@gmail.com>
2023-02-18 21:55:33 +0800

    fix style
    making the font size small for desktop view and smaller on mobile view

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function Button({ children }: Props) {
   return (
-    <button className="py-2 px-4 border rounded-full bg-green-600 text-white uppercase hover:bg-green-500 text-xs md:text-base">
+    <button className="py-2 px-4 border rounded-full bg-green-600 text-white uppercase hover:bg-green-500 text-xs md:text-sm">
         {children}
     </button>
   )
