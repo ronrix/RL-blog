817eecb53afa54921421a9a8aef0438f45b463fa
ronrix <medyochain@gmail.com>
2023-02-18 21:56:13 +0800

    fix style
    fixing search input style and sign in button. changes the color of signin button to black and making the search input smaller

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -12,8 +12,8 @@ export default function Header() {
           
             {/* desktop search */}
             <div className="ml-5 hidden sm:flex item-center rounded-full px-5 bg-gray-100">
-                <img src={search} alt="this is an image of a search icon" />
-                <input type="text" placeholder='Search RL' className="ml-3 bg-transparent border-none outline-none" />
+                <img src={search} alt="this is an image of a search icon" className="w-[10%]" />
+                <input type="text" placeholder='Search RL' className="ml-3 bg-transparent outline-none text-sm" />
             </div> 
         </div>
 
@@ -25,7 +25,7 @@ export default function Header() {
             </Link>
 
             <Button>Sign up</Button>
-            <Link to="/signin" className="px-4 uppercase font-['Manrope'] text-xs md:text-base">Sign in</Link>
+            <Link to="/signin" className="px-4 uppercase font-['Manrope'] text-xs md:text-sm text-black">Sign in</Link>
         </div>
     </div>
   )
