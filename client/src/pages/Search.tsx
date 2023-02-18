7ff9d40bc2c963242d097cd4c769a7eaf91f82d5
ronrix <medyochain@gmail.com>
2023-02-18 21:57:23 +0800

    adding search page

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 export default function Search() {
   return (
-    <div>Search</div>
+    <div>
+      <Header />
+      <div className="container mx-auto h-screen p-5 md:p-0">
+        <h2 className="">Recent Searches</h2>
+
+        {/* searches */}
+        <div className="mt-10">
+          <h3>You have no recent searches</h3>
+        </div>
+      </div>
+      <Footer />
+    </div>
   )
 }
