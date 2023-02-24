ecf015e9b87b8522a32a7e085d5bf63d9ddffbf3
ronrix <medyochain@gmail.com>
2023-02-24 20:40:36 +0800

    style
    move this info component to the top instead of showing from the bottom

--- client/src/components/Info/index.tsx
+++ client/src/components/Info/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Info() {
   return (
-    <div className='absolute whitespace-nowrap -bottom-14 left-0 bg-white p-3 shadow-xl border after:content-[""] after:absolute after:left-1 after:w-2 after:h-2 after:-top-1 after:rotate-45 after:bg-white after:border after:border-r-0 after:border-b-0'>
+    <div className='absolute rounded-lg whitespace-nowrap -top-14 right-full bg-white p-3 shadow-xl border after:content-[""] after:absolute after:right-1 after:w-2 after:h-2 after:-bottom-1 after:rotate-45 after:bg-white after:border after:border-l-0 after:border-t-0'>
       <h5 className='text-sm'>
         To learn more about markdown syntax 
         <a target={"_blank"} href="https://www.markdownguide.org/basic-syntax/" className="ml-1">markdown syntax</a>
