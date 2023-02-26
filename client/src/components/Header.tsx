590e8243456921def0b3ac8f99058f3a29275f69
ronrix <medyochain@gmail.com>
2023-02-26 14:09:46 +0800

    rename the import location filename of useRetrieve file

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import useRetrieveUser from '../hooks/useRetireiveUser';
+import useRetrieveUser from '../hooks/useRetrieve';
 import AuthModal from './AuthModal';
 import Avatar from './Avatar';
 import Button from './Button';
